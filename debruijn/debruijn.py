6715198f156e79e4fd1910814e8490f6eeab0ac1
GrosMax <maximekermarrec14@gmail.com>
2020-10-18 23:19:04 +0200

    quatrième commit

--- debruijn/debruijn.py
+++ debruijn/debruijn.py
@@ -18,13 +18,11 @@ import os
 import sys
 import networkx as nx
 import networkx.drawing
-import matplotlib
 from operator import itemgetter
 import random
 random.seed(9001)
 from random import randint
 import statistics
-import matplotlib.pyplot as plt
 import hashlib
 import pickle
 
@@ -93,42 +91,37 @@ def build_kmer_dict(fichier, k):
                 dico[kmer] = dico[kmer] + 1
             else:
                 dico[kmer] = 1
-    #print(dico)
     return dico
 
 
 def build_graph(dico):
-    G = nx.DiGraph()
+    graphe = nx.DiGraph()
     for key in dico:
         prefixe = str(key)[:-1]
         suffixe = str(key)[1:]
-        G.add_edge(prefixe, suffixe, weight=int(dico[key]))
-    for n, nbrs in G.adj.items():
-        for nbr, eattr in nbrs.items():
-            wt = eattr['weight']
-            if wt > 0.5: pass#print(f"({n}, {nbr}, {wt})")
+        graphe.add_edge(prefixe, suffixe, weight=int(dico[key]))
     
-    return(G)
+    return(graphe)
 
-def get_starting_nodes (G):
+def get_starting_nodes (graphe):
     entree = []    
-    for node in G.nodes:
-        if list(G.predecessors(node)) == []:
+    for node in graphe.nodes:
+        if list(graphe.predecessors(node)) == []:
             entree.append(node)
     return entree
 
-def get_sink_nodes (G):
+def get_sink_nodes (graphe):
     sortie = []    
-    for node in G.nodes:
-        if list(G.successors(node)) == []:
+    for node in graphe.nodes:
+        if list(graphe.successors(node)) == []:
             sortie.append(node)
     return sortie
 
-def get_contigs (G,entree,sortie):
+def get_contigs (graphe,entree,sortie):
     list_contig = []
     for i in entree:
         for j in sortie:
-            for path in nx.all_simple_paths(G, i, j):
+            for path in nx.all_simple_paths(graphe, i, j):
                 contig = path[0][:1]
                 for k in range(1, len(path)):
                     if k == len(path)-1:
@@ -155,25 +148,30 @@ def fill(text, width=80):
 def std (liste):
     return statistics.stdev(liste)
 
-def path_average_weight (G, chemin):
+def path_average_weight (graphe, chemin):
     poids = []
     for i in range(0, len(chemin)-1):
-        poids.append(G[chemin[i]][chemin[i+1]]["weight"])
+        poids.append(graphe[chemin[i]][chemin[i+1]]["weight"])
     return statistics.mean(poids)
 
+def path_weight(graphe, chemin):
+    poids = 0
+    for i in range(0, len(chemin)-1):
+        poids += graphe[chemin[i]][chemin[i+1]]["weight"]
+    return poids
 
 
-def remove_paths (G, chemin, delete_entry_node, delete_sink_node):
+def remove_paths (graphe, chemin, delete_entry_node, delete_sink_node):
     for i in chemin:
         for j in range(1, len(i)-1):
-            G.remove_node(i[j])
+            graphe.remove_node(i[j])
         if delete_entry_node == True:
-            G.remove_node(i[0])
+            graphe.remove_node(i[0])
         if delete_sink_node == True:
-            G.remove_node(i[len(i)-1])
-    return G
+            graphe.remove_node(i[len(i)-1])
+    return graphe
 
-def select_best_path (G, chemin, long_chemin, poids_chemin, delete_entry_node = False, delete_sink_node = False):
+def select_best_path (graphe, chemin, long_chemin, poids_chemin, delete_entry_node = False, delete_sink_node = False):
     best_poids = max(poids_chemin)
     best_long = max(long_chemin)
     index_poids = []
@@ -188,28 +186,26 @@ def select_best_path (G, chemin, long_chemin, poids_chemin, delete_entry_node = 
         bon_chemin = chemin[index_poids[0]]
         del chemin[index_poids[0]]
         chemin = conservation_nodes(bon_chemin, chemin)
-        G = remove_paths (G, chemin, delete_entry_node, delete_sink_node)
-        return G
+        graphe = remove_paths (graphe, chemin, delete_entry_node, delete_sink_node)
+        return graphe
     if len(index_taille) == 1:
         bon_chemin = index_taille[0]
         del chemin[index_taille[0]]
         chemin = conservation_nodes(bon_chemin, chemin)
-        G = remove_paths (G, chemin, delete_entry_node, delete_sink_node)
-        return G
+        graphe = remove_paths (graphe, chemin, delete_entry_node, delete_sink_node)
+        return graphe
     else : 
         hasard = random.randint(0,len(index_taille)-1)
         bon_chemin = chemin[hasard]
         del chemin[hasard]
         chemin = conservation_nodes(bon_chemin, chemin)
-        G = remove_paths (G, chemin, delete_entry_node, delete_sink_node)
-        return G
+        graphe = remove_paths (graphe, chemin, delete_entry_node, delete_sink_node)
+        return graphe
 
 def conservation_nodes (conserve, suppr):
     definitive_list = []
     a_suppr = []
     for path in suppr:
-        print(conserve)
-        print(path)
         a_suppr.append(list(set(path) - set(conserve)))
     nouveau_suppr = []
     nouveau_suppr.append(conserve[0])
@@ -217,40 +213,52 @@ def conservation_nodes (conserve, suppr):
         for path_2 in path_1:
             nouveau_suppr.append(path_2)
     nouveau_suppr.append(conserve[-1])
+    
+    nouveau_suppr = unique(nouveau_suppr)
+    
     definitive_list.append(nouveau_suppr)
-    print(definitive_list)
     return definitive_list
 
+def unique(list1): 
+  
+    # intilize a null list 
+    unique_list = [] 
+      
+    for x in list1: 
+        # check if exists in unique_list or not 
+        if x not in unique_list: 
+            unique_list.append(x) 
+    return unique_list
 
-
-def solve_bubble (G, ancestor, descendant):
-    pathways = nx.all_simple_paths(G, ancestor, descendant, cutoff=None)
+def solve_bubble (graphe, ancestor, descendant):
+    pathways = nx.all_simple_paths(graphe, ancestor, descendant, cutoff=None)
     poids_chemin = []
     long_chemin = []
     chemin = []
     for path in pathways:
         chemin.append(path)
-        poids_chemin.append(path_average_weight(G, path))
+        poids_chemin.append(path_average_weight(graphe, path))
         long_chemin.append(len(path))
-    select_best_path (G, chemin, long_chemin, poids_chemin, delete_entry_node = False, delete_sink_node = False)
-    return G
+    select_best_path (graphe, chemin, long_chemin, poids_chemin, delete_entry_node = False, delete_sink_node = False)
+    return graphe
 
 
-def simplify_bubbles (G):
+def simplify_bubbles (graphe):
     ensemble_chemin = []
     ensemble_ancestre = []
     ensemble_descendant = []
-    for node_A in G.nodes:
-        for node_B in G.nodes:
+    start = get_starting_nodes (graphe)
+    end = get_sink_nodes(graphe)
+    for node_A in start:
+        for node_B in end:
             if node_A != node_B:
-                chemin = nx.all_simple_paths(G, node_A, node_B)
+                chemin = nx.all_simple_paths(graphe, node_A, node_B)
                 if len(list(chemin)) > 1:
-                    chemin = nx.all_simple_paths(G, node_A, node_B)
+                    chemin = nx.all_simple_paths(graphe, node_A, node_B)
                     ensemble_ancestre.append(node_A)
                     ensemble_descendant.append(node_B)
                     for path in chemin:
                         ensemble_chemin.append(list(chemin))
-    print(ensemble_chemin)
     i=0
     save_path = 0
     save_long_path = 0
@@ -261,13 +269,49 @@ def simplify_bubbles (G):
                 save_long_path = len(path_2)
                 ancestre = path_2[0]
                 descendant = path_2[-1]
-    G = solve_bubble(G, ancestre, descendant)
-    return G
+    graphe = solve_bubble(graphe, ancestre, descendant)
+    return graphe
 
+def deter_min(ensemble_chemin):
+    min = [ensemble_chemin[0]]
+    taille = len(ensemble_chemin[0])
+    for chemin in ensemble_chemin:
+        if len(chemin) < taille:
+            taille = len(chemin)
+            min = chemin
+    return min
 
-def solve_entry_tips ():
+def solve_entry_tips (graphe, entree):
+    ensemble_chemin = []
+    ensemble_poids = []
+    ensemble_taille = []
+    starting_nodes = get_sink_nodes (graphe)
+    for start in starting_nodes:
+        for node in entree:
+            chemin = list(nx.all_simple_paths(graphe, node, start))
+            for path in chemin:
+                ensemble_chemin.append(path)
+    for chemins in ensemble_chemin:
+        ensemble_poids.append(path_weight(graphe, chemins))
+        ensemble_taille.append(len(chemins))
+    graphe = select_best_path (graphe, ensemble_chemin, ensemble_taille, ensemble_poids, delete_entry_node = False, delete_sink_node = False)
+    return(graphe)
 
-#def solve_out_tips ():
+def solve_out_tips (graphe, sortie):
+    ensemble_chemin = []
+    ensemble_poids = []
+    ensemble_taille = []
+    starting_nodes = get_starting_nodes (graphe)
+    for start in starting_nodes:
+        for node in sortie:
+            chemin = list(nx.all_simple_paths(graphe, start, node))
+            for path in chemin:
+                ensemble_chemin.append(path)
+    for chemins in ensemble_chemin:
+        ensemble_poids.append(path_weight(graphe, chemins))
+        ensemble_taille.append(len(chemins))
+    graphe = select_best_path (graphe, ensemble_chemin, ensemble_taille, ensemble_poids, delete_entry_node = False, delete_sink_node = False)
+    return(graphe)
 
 
 #==============================================================
@@ -279,32 +323,38 @@ def main():
     Main program function
     """
 
+
     # Get arguments and using it
 
-    graph_1 = nx.DiGraph()
-    graph_1.add_weighted_edges_from([(1, 2, 10), (3, 2, 2), (2, 4, 15), (4, 5, 15)])
-    graph_1 = solve_entry_tips(graph_1, [1, 3])  
-    print( (3, 2) not in graph_1.edges())
-    print( (1, 2) in graph_1.edges())
-    graph_2 = nx.DiGraph()
-    graph_2.add_weighted_edges_from([(1, 2, 2), (6, 3, 2), (3, 2, 2),
-                                     (2, 4, 15), (4, 5, 15)])
-    graph_2 = solve_entry_tips(graph_2, [1, 6])  
-    print( (1, 2) not in graph_2.edges())
-    print( (6, 3) in graph_2.edges())
-    print( (3, 2) in graph_2.edges())
+    args = get_arguments()
+    dico = build_kmer_dict(args.fastq_file, args.kmer_size)
 
-    graph_1 = nx.DiGraph()
-    graph_1.add_weighted_edges_from([(1, 2, 15), (2, 3, 15), (3, 4, 15), (4, 5, 15), (4, 6, 2)])
-    graph_1 = solve_out_tips(graph_1, [5, 6])  
-    print( (4, 6) not in graph_1.edges())
-    print( (4, 5) in graph_1.edges()  )
-    graph_2 = nx.DiGraph()
-    graph_2.add_weighted_edges_from([(1, 2, 15), (2, 3, 15), (3, 4, 15), (4, 5, 2), (4, 6, 2) , (6, 7, 2)])
-    graph_2 = solve_out_tips(graph_2, [5, 7])  
-    print( (4, 5) not in graph_2.edges())
-    print( (6, 7) in graph_2.edges())
+    graphe = build_graph(dico)
+
+
+    graphe = simplify_bubbles (graphe)
     
+    node_entree = []
+
+    for node_A in graphe.nodes:
+        if len(list(graphe.predecessors(node_A))) >= 2:
+            graphe.predecessors(node)
+            node_entree.append(list(node_A))
+    if len(node_entree) != 0:
+        graphe = solve_entry_tips(graphe, node_entree)
+
+    node_sortie = []
+    for node_B in graphe.nodes:
+        if len(list(graphe.predecessors(node_B))) >= 2:
+            node_sortie.append(list(node_B))
+
+    if len(node_sortie) != 0:
+        graphe = solve_entry_tips(graphe, node_sortie)
+
+    entree = get_starting_nodes(graphe)
+    sortie = get_sink_nodes(graphe)
+    list_contig = get_contigs(graphe, entree, sortie)
+    save_contigs (list_contig, args.output_file)
 
 if __name__ == '__main__':
     main()
